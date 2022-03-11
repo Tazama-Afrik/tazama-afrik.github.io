8f612c3c524d557d4d17df658082e4656d7c891a
Mogakamo <mogaka.amo254@gmail.com>
2022-03-11 10:08:38 +0300

    Making changes in the main repo

--- js.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const africanCountriesAPI = require('@babajidemm/african-countries-api');
-const countryData = africanCountriesAPI.byName('Nigeria');
-console.log(countryData.body.toString());
-// const africanCountriesAPI = require('@babajidemm/african-countries-api');
-// const countriesByName = africanCountriesAPI.byName('Ghana');
-// console.log(countriesByName.body.toString());

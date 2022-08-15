651500e30ebb1744cae1a9935125916b52d6717c
Obed101 <obedamoako92@gmail.com>
2022-08-15 08:44:14 +0000

    completed link validation

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
@@ -18,4 +17,4 @@ def message():
     return render_template('index.html', name=name, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=true, port=5000)
+    app.run(debug=True, port=5000)
